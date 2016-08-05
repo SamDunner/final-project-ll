4b46489118e452932738e4f75d6febcebba397f8
munens <kaumbuthom@gmail.com>
2016-08-05 23:19:35 +0000

    Created working form fields

--- public/client/src/signup/Signup.jsx
+++ public/client/src/signup/Signup.jsx
@@ -2,14 +2,41 @@ import React, {Component} from 'react';
 
 const Signup = React.createClass({
 
+	getInitialState: function() {
+		return { first_name: "", last_name: "", email: "", username: "", password: "" };
+	},
+
 	componentDidMount : function() {
 
 	},
 
+	handleFieldChange : function(event) {
+		this.setState({first_name: event.target.value, 
+					   last_name: event.target.value,
+					   email: event.target.value,
+					   username: event.target.value,
+					   password: event.target.value	})
+	},
+
+
 	render: function() {
 		return (
 			
-			<form />
+			<div id="registration-form">
+			<form className="register">
+				<label> First name: <input type="text" name="firstname" value={this.state.value} onChange={this.handleFieldChange} /> </label>
+				<br/>
+				<label> Last name: <input type="text" name="lastname" value={this.state.value} onChange={this.handleFieldChange} /> </label>
+				<br/>
+				<label> Email: <input type="text" name="email" value={this.state.value} onChange={this.handleFieldChange} /> </label>
+				<br/>
+				<label> Username: <input type="text" name="username" value={this.state.value} onChange={this.handleFieldChange} /> </label>
+				<br/>
+				<label> Password: <input type="text" name="password" value={this.state.value} onChange={this.handleFieldChange} /> </label>
+				<br/>
+				<button className='btn-submit'>Register</button>
+			</form>
+			</div>
 
     	);
 	}
