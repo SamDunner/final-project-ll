0ee97748e321027cbc484a59b7fabc660c76db99
sam dunner <samdunner23@gmail.com>
2016-08-05 02:18:05 +0000

    Add put/delete routes for users

--- routes/users.js
+++ routes/users.js
@@ -18,12 +18,47 @@ module.exports = (knex) => {
     knex
       .select("*")
       .from("users")
-      .where('id', params.id)
+      .where('id', req.params.id)
       .then((results) => {
         res.json(results);
-        console.log("tests");
+        console.log(results);
     });
   });
+
+  router.post("/", (req, res) => {
+    knex("users").insert({
+      'first_name': req.body.first_name,
+      'last_name': req.body.last_name,
+      'username': req.body.username,
+      'email': req.body.email,
+      'password': req.body.password
+    })
+    .then((results) => {
+      console.log("user posted")
+    });
+  });
+
+  router.put("/:id", (req, res) => {
+    knex("users").where('id', req.params.id)
+    .update({
+      'first_name': req.body.first_name,
+      'last_name': req.body.last_name,
+      'username': req.body.username,
+      'email': req.body.email,
+      'password': req.body.password
+    })
+    .then((results) => {
+      console.log("user updated")
+    });
+  });
+
+  router.delete("/:id", (req, res) => {
+    knex("users").where('id', req.params.id)
+    .del().then((results) => {
+      console.log("user deleted")
+    });
+  });
+
 
   return router;
 }
