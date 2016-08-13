88bf62a6ad1f035f83df4ea93518660cbdd8db35
sam dunner <samdunner23@gmail.com>
2016-08-13 22:41:58 +0000

    Tool tip added but needs editing

--- public/client/src/navbar/NavBar.jsx
+++ public/client/src/navbar/NavBar.jsx
@@ -30,19 +30,23 @@ const NavBar = React.createClass({
     return (
 
       <div className="standard-nav-bar col-md-12 col-lg-12">
-        <a className="company-logo col-md-offset-1 col-md-2 col-lg-2 btn btn-danger"
+        <a className="company-logo col-md-offset-1 col-md-2 col-lg-2 btn btn-outline-primary"
            href="/">
              Home(future logo)
         </a>
-        <Link className="btn btn-danger col-md-2 col-lg-2 col-md-offset-2"
+        <Link className="btn btn-outline-primary col-md-2 col-lg-2 col-md-offset-2"
               to={"/users/" + this.getCookie() }>
                  My Profile
         </Link>
         <Link className="fa fa-plus-square-o fa-3x col-md-1 col-lg-1 col-md-offset-1"
               aria-hidden="true"
-              to={"/users/" + this.getCookie() + "/create"}>
+              to={"/users/" + this.getCookie() + "/create"}
+              data-toggle="tooltip"
+              data-placement="top"
+              title="Create new ma"
+              id="example">
         </Link>
-        <Link className="btn btn-danger col-md-2 col-lg-2"
+        <Link className="btn btn-outline-primary col-md-2 col-lg-2"
               to="/"
               onClick={this.onLogout}>
           Log out
